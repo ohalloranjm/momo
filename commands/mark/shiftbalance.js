c4dcf156b39da2b8a1a8df4be73e8495f3fe8390
ohalloranjm <johalloran@protonmail.com>
2024-08-01 20:55:39 -0700

    buttons up, not working yet

--- commands/mark/shiftbalance.js
+++ commands/mark/shiftbalance.js
@@ -1,25 +1,106 @@
 const { PlayerCharacter } = require('../../database/models');
-const { SlashCommandBuilder } = require("discord.js");
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  SlashCommandBuilder,
+} = require('discord.js');
 
 module.exports = {
-    alias: 'b',
-    data: new SlashCommandBuilder()
-        .setName('shiftbalance')
-        .setDescription('Shift your balance.')
-        .addStringOption(option => 
-            option.setName('principle')
-                .setDescription('The principle or direction to shift your balance toward.')
-        ),
-    async execute(interaction) {
-        await interaction.deferReply();
-        const pc = await PlayerCharacter.fetch(interaction, {
-            info: 'balance'
-        })
-        await interaction.followUp(`Your balance is ${pc.balance}, your center is ${pc.center}, and your playbook is The ${pc.playbook.capitalize()}`);
+  alias: 'b',
+  data: new SlashCommandBuilder()
+    .setName('shiftbalance')
+    .setDescription('Shift your balance.')
+    .addStringOption(option =>
+      option
+        .setName('principle')
+        .setDescription(
+          'The principle or direction to shift your balance toward.'
+        )
+    ),
+  async execute(interaction) {
+    await interaction.deferReply();
 
-        const principle = interaction.options.getString('principle');
-        if (!principle) await pc.shiftBalance([])
+    const pc = await PlayerCharacter.fetch(interaction, {
+      info: 'balance',
+    });
 
-        console.log(principle);
+    let target = interaction.options.getString('principle') || 'away';
+    const { principles } = pc.getPlaybook();
+
+    // defaults to shifting away from center
+    let shift = await pc.shiftBalance(target);
+
+    // if balance is at center and no principle is supplied, ask for principle and try again
+    if (shift.status === 'need-principle') {
+      const row = new ActionRowBuilder();
+      const leftButton = new ButtonBuilder()
+        .setCustomId('left')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel(`${principles[0].capitalize()} (${(-pc.balance).sign()})`);
+      const rightButton = new ButtonBuilder()
+        .setCustomId('right')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel(`${principles[1].capitalize()} (${pc.balance.sign()})`);
+      row.addComponents(leftButton, rightButton);
+      const click = await interaction.followUp({
+        content:
+          'Your balance is currently at your center. Choose a principle to shift toward.',
+        components: [row],
+      });
+
+      const collectorFilter = i => i.user.id === interaction.user.id;
+
+      try {
+        const confirmPrinciple = await click.awaitMessageComponent({
+          filter: collectorFilter,
+          time: 10_000,
+        });
+
+        console.log(confirmPrinciple.customId);
+
+        shift = await pc.shiftBalance(confirmPrinciple.customId);
+
+        await interaction.editReply({
+          content: 'Hell yeah.',
+          components: [],
+        });
+      } catch (e) {
+        console.error(e);
+        await interaction.editReply({
+          content: 'Confirmation not received within 1 minute, cancelling',
+          components: [],
+        });
+      }
     }
-}
+
+    // if shifting causes loss of balance, check whether it's between sessions
+    if (shift.status === 'lose-balance') {
+      const row = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId('lose')
+          .setStyle(ButtonStyle.Primary)
+          .setLabel(
+            `Lose Balance toward ${
+              pc.balance < 0 ? principles[0] : principles[1]
+            }`,
+            new ButtonBuilder()
+              .setCustomId('keep')
+              .setStyle(ButtonStyle.Secondary)
+              .setLabel("Don't Lose Balance")
+          )
+      );
+      const leftButton = new ButtonBuilder()
+        .setCustomId('left')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel(`${principles[0].capitalize()} (${(-pc.balance).sign()})`);
+      const rightButton = new ButtonBuilder()
+        .setCustomId('right')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel(`${principles[1].capitalize()} (${pc.balance.sign()})`);
+      row.addComponents(leftButton, rightButton);
+    }
+
+    console.log(shift);
+  },
+};
--- database/models/playercharacter.js
+++ database/models/playercharacter.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 
 require('../../functions');
 const playbooks = require('../../playbooks');
@@ -10,20 +8,19 @@ const { conditions, STATS, STATUSES, TRAININGS } = require('../../constants');
 
 module.exports = (sequelize, DataTypes) => {
   class PlayerCharacter extends Model {
-
     static associate(models) {
       PlayerCharacter.belongsToMany(models.Technique, {
         through: models.PcTechnique,
         foreignKey: 'pcId',
-        otherKey: 'techniqueId'
-      })
-    };
+        otherKey: 'techniqueId',
+      });
+    }
 
     static async fetch(interaction, options) {
       let query = this.findOne.bind(this);
 
-      const attributes = [ 'id', 'name' ];
-      
+      const attributes = ['id', 'name'];
+
       const { id: userId } = interaction.user;
       const where = { userId, active: true };
 
@@ -37,32 +34,41 @@ module.exports = (sequelize, DataTypes) => {
       // { roster: true }: return an array of all the user's PCs, not just active ones
       if (roster) {
         query = this.findAll.bind(this);
-        where.active = { [Op.or]: [ true, false ] };
+        where.active = { [Op.or]: [true, false] };
       }
 
       // { allInfo: true }: return all attributes, not just name
       if (allInfo) return await query({ where });
 
-
       // { info: string (or array of strings) }: adds attribute(s) to the query
 
       // shortcuts to add multiple attributes at once
       const attributeLookup = {
-        advancement: [ 'growth', 'advanceMoveOwn', 'advanceMoveOther', 'advanceCenter', 'advanceMomentBalance', 'advanceCreativity', 'advanceFocus', 'advanceHarmony', 'advancePassion'],
-        balance: [ 'playbook', 'balance', 'center' ],
+        advancement: [
+          'growth',
+          'advanceMoveOwn',
+          'advanceMoveOther',
+          'advanceCenter',
+          'advanceMomentBalance',
+          'advanceCreativity',
+          'advanceFocus',
+          'advanceHarmony',
+          'advancePassion',
+        ],
+        balance: ['playbook', 'balance', 'center'],
         conditions: conditions.QUERY,
         stats: STATS,
         statuses: STATUSES,
         training: TRAININGS,
-      }
+      };
 
-      const attributePush = (attributeKey) => {
+      const attributePush = attributeKey => {
         if (attributeKey in attributeLookup) {
-            attributes.push(...attributeLookup[attributeKey])
+          attributes.push(...attributeLookup[attributeKey]);
         } else {
-            attributes.push(attributeKey);
+          attributes.push(attributeKey);
         }
-      }
+      };
 
       if (typeof info === 'string') {
         attributePush(info);
@@ -71,31 +77,41 @@ module.exports = (sequelize, DataTypes) => {
         info.forEach(attributePush);
       }
 
-      return await query(queryOptions);  
+      return await query(queryOptions);
     }
 
     // returns an array of marked conditions
     // toggle user view to output a string instead
     conditionList(userView) {
-      const conditionNames = this.playbook === 'elder' ? conditions.ELDER : conditions.DEFAULT;
+      const conditionNames =
+        this.playbook === 'elder' ? conditions.ELDER : conditions.DEFAULT;
 
-      const conditionMarks = [ this.conditionA, this.conditionB, this.conditionC, this.conditionD, this.conditionE ]
+      const conditionMarks = [
+        this.conditionA,
+        this.conditionB,
+        this.conditionC,
+        this.conditionD,
+        this.conditionE,
+      ];
 
       const list = conditionNames.filter((_c, i) => conditionMarks[i]);
 
       if (!userView) return list;
       if (!list.length) return 'No Conditions';
-      return list.map(condition => condition.capitalize()).join(', ')
-      
+      return list.map(condition => condition.capitalize()).join(', ');
     }
 
     // return the total number of conditions marked (integer from 0 to 5)
     conditionsMarked() {
-
-      const conditions = [ this.conditionA, this.conditionB, this.conditionC, this.conditionD, this.conditionE ]
+      const conditions = [
+        this.conditionA,
+        this.conditionB,
+        this.conditionC,
+        this.conditionD,
+        this.conditionE,
+      ];
 
       return conditions.reduce((sum, condition) => +condition + sum, 0);
-
     }
 
     // shiftBalance
@@ -103,14 +119,16 @@ module.exports = (sequelize, DataTypes) => {
       steps = isNaN(steps) ? 1 : +steps;
 
       const { principles } = this.getPlaybook();
-      const result = { principles, priorBalance: this.balance }
+      const result = { principles, priorBalance: this.balance };
 
       if (this.balance === this.center) {
-
         if (principle === 'away') {
           result.newBalance = this.balance;
           result.status = 'need-principle';
-          return result
+          result.callback = function (newPrinciple) {
+            return this.shiftBalance(newPrinciple, steps);
+          };
+          return result;
         }
 
         if (principle === 'center') {
@@ -123,13 +141,13 @@ module.exports = (sequelize, DataTypes) => {
       const principleLookup = {
         [principles[0].toLowerCase()]: -1,
         [principles[1].toLowerCase()]: 1,
-        'left': -1,
-        'right': 1,
-        'away': this.balance > this.center ? 1 : -1,
-        'center': this.balance > this.center ? -1 : 1,
+        left: -1,
+        right: 1,
+        away: this.balance > this.center ? 1 : -1,
+        center: this.balance > this.center ? -1 : 1,
         '-1': -1,
-        '1': 1
-      }
+        1: 1,
+      };
 
       if (!(principle.toLowerCase() in principleLookup)) {
         result.newBalance = this.balance;
@@ -140,17 +158,17 @@ module.exports = (sequelize, DataTypes) => {
       const direction = principleLookup[principle];
 
       const shift = direction * steps;
-      const targetBalance = this.balance + shift; 
-      
+      const targetBalance = this.balance + shift;
+
       if (targetBalance < -3) {
         this.balance = -3;
-        result.status = 'lose-balance'
+        result.status = 'lose-balance';
       } else if (targetBalance > 3) {
         this.balance = 3;
-        result.status = 'lose-balance'
+        result.status = 'lose-balance';
       } else {
         this.balance = targetBalance;
-        result.status = 'shifted-balance'
+        result.status = 'shifted-balance';
       }
 
       await this.save();
@@ -158,289 +176,317 @@ module.exports = (sequelize, DataTypes) => {
       result.newBalance = this.balance;
 
       return result;
-
     }
 
     trainingList(userView) {
-      const trainings = ['waterbending', 'earthbending', 'firebending', 'airbending', 'weapons', 'technology']
+      const trainings = [
+        'waterbending',
+        'earthbending',
+        'firebending',
+        'airbending',
+        'weapons',
+        'technology',
+      ];
       const list = trainings.filter(training => this[training]);
 
       if (!userView) return list;
       if (!list.length) return 'Error: No training found';
-      return list.map(training => training.capitalize()).join(', ')
+      return list.map(training => training.capitalize()).join(', ');
     }
 
     getPlaybook() {
-      return playbooks[this.playbook]
+      return playbooks[this.playbook];
     }
-    
   }
-  
-  PlayerCharacter.init({
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+
+  PlayerCharacter.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      playbook: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [
+            [
+              'adamant',
+              'bold',
+              'guardian',
+              'hammer',
+              'icon',
+              'idealist',
+              'pillar',
+              'prodigy',
+              'rogue',
+              'successor',
+              'destined',
+              'elder',
+              'foundling',
+              'razor',
+              'adrift',
+              'aspirant',
+              'outcast',
+            ],
+          ],
+        },
+      },
+      creativity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: -2,
+          max: 3,
+        },
+      },
+      focus: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: -2,
+          max: 3,
+        },
+      },
+      harmony: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: -2,
+          max: 3,
+        },
+      },
+      passion: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: -2,
+          max: 3,
+        },
+      },
+      balance: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: -3,
+          max: 3,
+        },
+      },
+      center: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: -2,
+          max: 3,
+        },
+      },
+      waterbending: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      earthbending: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      firebending: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      airbending: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      weapons: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      technology: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      backgrounds: {
+        type: DataTypes.STRING,
+      },
+      demeanor: {
+        type: DataTypes.STRING,
+      },
+      fatigue: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+          max: 5,
+        },
+      },
+      conditionA: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      conditionB: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      conditionC: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      conditionD: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      conditionE: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      empowered: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      favored: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      inspired: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      prepared: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      doomed: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      impaired: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      trapped: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      stunned: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      growth: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+          max: 4,
+        },
+      },
+      advanceMoveOwn: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+          max: 2,
+        },
+      },
+      advanceMoveOther: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+          max: 2,
+        },
+      },
+      advanceCenter: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+          max: 2,
+        },
+      },
+      advanceMomentBalance: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+          max: 2,
+        },
+      },
+      advanceCreativity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+        },
+      },
+      advanceFocus: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+        },
+      },
+      advanceHarmony: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+        },
+      },
+      advancePassion: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          min: 0,
+        },
+      },
     },
-    active: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    playbook: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isIn: [['adamant', 'bold', 'guardian', 'hammer', 'icon', 'idealist', 'pillar', 'prodigy', 'rogue', 'successor', 'destined', 'elder', 'foundling', 'razor', 'adrift', 'aspirant', 'outcast']]
-      }
-    },
-    creativity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: -2,
-        max: 3
-      }
-    },
-    focus: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: -2,
-        max: 3
-      }
-    },
-    harmony: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: -2,
-        max: 3
-      }
-    },
-    passion: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: -2,
-        max: 3
-      }
-    },
-    balance: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: -3,
-        max: 3
-      }
-    },
-    center: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: -2,
-        max: 3
-      }
-    },
-    waterbending: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    earthbending: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    firebending: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    airbending: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    weapons: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    technology: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    backgrounds: {
-      type: DataTypes.STRING,
-    },
-    demeanor: {
-      type: DataTypes.STRING,
-    },
-    fatigue: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-        max: 5
-      }
-    },
-    conditionA: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    conditionB: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    conditionC: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    conditionD: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    conditionE: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    empowered: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    favored: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    inspired: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    prepared: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    doomed: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    impaired: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    trapped: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    stunned: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    },
-    growth: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-        max: 4
-      }
-    },
-    advanceMoveOwn: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-        max: 2
-      }
-    },
-    advanceMoveOther: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-        max: 2
-      }
-    },
-    advanceCenter: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-        max: 2
-      }
-    },
-    advanceMomentBalance: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-        max: 2
-      }
-    },
-    advanceCreativity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-      }
-    },
-    advanceFocus: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-      }
-    },
-    advanceHarmony: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-      }
-    },
-    advancePassion: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      validate: {
-        min: 0,
-      }
-    },
-  }, {
-    sequelize,
-    modelName: 'PlayerCharacter',
-  });
+    {
+      sequelize,
+      modelName: 'PlayerCharacter',
+    }
+  );
   return PlayerCharacter;
 };
