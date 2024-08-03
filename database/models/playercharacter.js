64119bb16b02d478ac41540e85ee41ec98ff87e9
ohalloranjm <johalloran@protonmail.com>
2024-08-03 06:00:55 -0700

    added resetbalance command

--- database/models/playercharacter.js
+++ database/models/playercharacter.js
@@ -208,6 +208,23 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
 
+    // resetBalance
+    async resetBalance() {
+      const result = { oldBalance: this.balance };
+
+      if (this.balance === this.center) {
+        result.newBalance = this.balance;
+        result.message = `${this.name}’s balance is already at their center.`;
+        return result;
+      }
+
+      this.balance = this.center;
+      result.newBalance = this.center;
+      result.message = `${this.name} takes some time to recenter themself, returning their balance to center.`;
+      await this.save();
+      return result;
+    }
+
     trainingList(userView) {
       const trainings = [
         'waterbending',
