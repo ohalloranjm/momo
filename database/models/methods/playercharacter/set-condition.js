f77baa49bebb0be0525e354211ba1c14e94f1363
ohalloranjm <johalloran@protonmail.com>
2024-08-04 06:27:48 -0700

    finished setCondition method

--- database/models/methods/playercharacter/set-condition.js
+++ database/models/methods/playercharacter/set-condition.js
@@ -3,17 +3,28 @@ const { conditions } = require('../../../../constants');
 module.exports = {
   key: 'setCondition',
 
-  async value(condition, val, options) {
+  async value(condition, val, options = {}) {
     const playbookConditions =
       this.playbook === 'elder' ? conditions.ELDER : conditions.DEFAULT;
 
     if (!playbookConditions.includes(condition))
-      throw Error('Not a valid condition for this playbook.');
+      return {
+        message: `${condition} is not a valid condition for this playbook.`,
+      };
 
     const idx = playbookConditions.indexOf(condition);
 
     const conditionsArr = this.conditions.split('');
 
-    if (+conditionsArr[idx] === +val) return;
+    if (+conditionsArr[idx] === +val)
+      return { message: `${condition} is already ${val ? marked : cleared}.` };
+
+    conditionsArr[idx] = +val;
+    this.conditions = conditionsArr.join('');
+    if (options.autosave !== false) await this.save();
+
+    return {
+      message: `${this.name} marks ${condition}!`,
+    };
   },
 };
