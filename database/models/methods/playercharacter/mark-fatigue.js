8bfc2dcade451a6e1ecc02b44d81cfbadd747984
ohalloranjm <johalloran@protonmail.com>
2024-08-06 10:01:18 -0700

    added markFatigue and setFatigue methods to PC model

---
+++ database/models/methods/playercharacter/mark-fatigue.js
@@ -0,0 +1,27 @@
+module.exports = {
+  key: 'markFatigue',
+
+  async value(n) {
+    n = isNaN(n) || n < 1 ? 1 : parseInt(n);
+
+    const target = this.fatigue + n;
+
+    if (target <= 5) return await this.setFatigue(target);
+
+    const response = await this.setFatigue(5);
+    const spillover = target - 5;
+    response.message += ` and is forced to mark ${spillover} conditions.`;
+
+    const conditionsMarked = this.conditionsMarked();
+
+    if (spillover > conditionsMarked) {
+      response.message += ` ${this.name} loses their balance!`;
+      this.conditions = '11111';
+      await this.save();
+    } else {
+      response.message += ' Type ``/condition`` to mark them.';
+    }
+
+    return response;
+  },
+};
