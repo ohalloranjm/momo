4a81dc0bb445814cc94fc072b2f86b5ff60cce34
ohalloranjm <johalloran@protonmail.com>
2024-08-06 14:41:02 -0700

    added default No Player Character response to PC static property

---
+++ database/models/methods/playercharacter/responses.js
@@ -0,0 +1,7 @@
+module.exports = {
+  key: 'responses',
+  static: true,
+  value: {
+    nopc: 'There are no player characters associated with this account. Use ``/newpc`` to create one.',
+  },
+};
