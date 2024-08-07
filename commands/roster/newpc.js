7c3de88fcc23457e99566d93b58b9534b33d40ad
ohalloranjm <johalloran@protonmail.com>
2024-08-06 19:39:05 -0700

    first foray into chaining callbacks

--- commands/roster/newpc.js
+++ commands/roster/newpc.js
@@ -1,6 +1,56 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { PlayerCharacter } = require('../../database/models/');
-const Playbooks = require('../../playbooks');
+const {
+  ActionRowBuilder,
+  SlashCommandBuilder,
+  StringSelectMenuBuilder,
+  StringSelectMenuOptionBuilder,
+} = require('discord.js');
+
+const selectPlaybook = async function (interaction) {
+  const select = new StringSelectMenuBuilder()
+    .setCustomId('playbook')
+    .setPlaceholder('Choose a playbook');
+
+  const playbooks = require('../../playbooks');
+  for (const key of Object.keys(playbooks)) {
+    const playbook = playbooks[key];
+    select.addOptions(
+      new StringSelectMenuOptionBuilder()
+        .setLabel(playbook.name)
+        .setDescription(
+          `${playbook.principles[0]} vs. ${playbook.principles[1]}`
+        )
+        .setValue(playbook.key)
+    );
+  }
+
+  const row = new ActionRowBuilder().addComponents(select);
+
+  const response = await interaction.followUp({
+    content: 'Choose a playbook.',
+    components: [row],
+  });
+
+  const collectorFilter = i => i.user.id === interaction.user.id;
+  try {
+    const choice = await response.awaitMessageComponent({
+      filter: collectorFilter,
+      time: 60_000,
+    });
+
+    console.log(choice);
+
+    await choice.update({
+      content: `You have selected the ${choice.values[0]}.`,
+      components: [],
+    });
+  } catch (e) {
+    console.error(e);
+    await interaction.editReply({
+      content: 'Confirmation not received within 1 minute, cancelling',
+      components: [],
+    });
+  }
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,96 +61,15 @@ module.exports = {
         .setName('name')
         .setDescription("Your character's name.")
         .setRequired(true)
-    )
-    .addStringOption(option =>
-      option
-        .setName('playbook')
-        .setDescription('Choose your playbook.')
-        .setRequired(true)
-        .addChoices(
-          { name: 'The Hammer', value: 'Hammer' },
-          { name: 'The Elder', value: 'Elder' },
-          { name: 'The Icon', value: 'Icon' },
-          { name: 'The Bold', value: 'Bold' }
-        )
-    )
-    .addStringOption(option =>
-      option
-        .setName('training')
-        .setDescription('Choose a bending or non-bending martial training.')
-        .setRequired(true)
-        .addChoices(
-          { name: 'Waterbending', value: 'Waterbending' },
-          { name: 'Earthbending', value: 'Earthbending' },
-          { name: 'Firebending', value: 'Firebending' },
-          { name: 'Airbending', value: 'Airbending' },
-          { name: 'Weapons', value: 'Weapons' },
-          { name: 'Technology', value: 'Technology' }
-        )
-    )
-    .addStringOption(option =>
-      option
-        .setName('stat-increase')
-        .setDescription('Choose one of your stats to increase by +1 (max. +2).')
-        .setRequired(true)
-        .addChoices(
-          { name: 'Creativity', value: 'Creativity' },
-          { name: 'Focus', value: 'Focus' },
-          { name: 'Harmony', value: 'Harmony' },
-          { name: 'Passion', value: 'Passion' }
-        )
     ),
+
   async execute(interaction) {
     await interaction.deferReply({
       ephemeral: true,
     });
 
-    const { id } = interaction.user;
+    await selectPlaybook(interaction);
 
-    const otherPCs = await PlayerCharacter.grab(interaction, { roster: true });
-
-    if (otherPCs.length >= 5 && id !== process.env.ADMIN_ID) {
-      return await interaction.followUp(
-        'You are allowed a maximum of five player characters per Discord account. Please delete an existing PC before creating a new one.'
-      );
-    }
-
-    const playbook = Playbooks[interaction.options.getString('playbook')];
-    const { Creativity, Focus, Harmony, Passion } = playbook;
-
-    const newPC = PlayerCharacter.build({
-      userId: interaction.user.id,
-      name: interaction.options.getString('name'),
-      playbook: interaction.options.getString('playbook'),
-      Creativity,
-      Focus,
-      Harmony,
-      Passion,
-      backgrounds: 'Military, Wilderness',
-    });
-
-    const stat = interaction.options.getString('stat-increase');
-
-    if (newPC[stat] === 2) {
-      await interaction.followUp(
-        `${playbook.name} already has a ${
-          stat[0].toUpperCase() + stat.slice(1)
-        } of +2, so you can't increase it during character creation. Choose another stat, and manually increase it using the \`\`/changestat\`\` command.`
-      );
-    } else {
-      newPC[stat]++;
-    }
-
-    newPC[interaction.options.getString('training')] = true;
-    await newPC.save();
-
-    await interaction.followUp('Character successfully created!');
-
-    for (let i = 0; i < otherPCs.length; i++) {
-      const PC = otherPCs[i];
-      PC.active = false;
-      await PC.save();
-      console.log(PC);
-    }
+    return await interaction.followUp('Howdy');
   },
 };
