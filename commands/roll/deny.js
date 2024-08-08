b5fdc3ea97bd6306e0d48b5d1164cb0e390e1463
ohalloranjm <johalloran@protonmail.com>
2024-08-08 09:27:05 -0700

    updated deny command to accept/prompt for balance principles

--- commands/roll/deny.js
+++ commands/roll/deny.js
@@ -1,4 +1,9 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  SlashCommandBuilder,
+} = require('discord.js');
 const Roll = require('../../classes/roll.js');
 const { PlayerCharacter } = require('../../database/models');
 
@@ -27,25 +32,86 @@ module.exports = {
     ),
 
   async execute(interaction) {
-    const target = interaction.options.getString('principle');
-    await interaction.deferReply({ ephemeral: !target });
+    const override = interaction.options.getInteger('override-modifier');
 
-    const pc = await PlayerCharacter.grab(interaction, [
-      'conditions',
-      'balance',
-    ]);
+    let pc;
+    if (override === null) {
+      pc = await PlayerCharacter.grab(interaction, ['conditions', 'balance']);
+    }
 
-    const roll = new Roll().addModifier(
-      interaction.options.getInteger('modifier')
-    );
+    let target = interaction.options.getString('principle');
+    console.log(target);
+    await interaction.deferReply({ ephemeral: !!pc && !target });
+
+    let balanceMod;
+    if (pc && !target) {
+      require('../../utils/custom-methods.js');
+      const { principles } = pc.getPlaybook();
+      const leftButton = new ButtonBuilder()
+        .setCustomId('left')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel(`${principles[0]} (${(-pc.balance).sign()})`);
+      const rightButton = new ButtonBuilder()
+        .setCustomId('right')
+        .setStyle(ButtonStyle.Primary)
+        .setLabel(`${principles[1]} (${pc.balance.sign()})`);
+      const row = new ActionRowBuilder().addComponents(leftButton, rightButton);
+      const sentButtons = await interaction.followUp({
+        content: 'What balance principle are you being called out on?',
+        components: [row],
+      });
+
+      const confirmPrinciple = await sentButtons.awaitMessageComponent({
+        filter: i => i.user.id === interaction.user.id,
+        time: 60_000,
+      });
+
+      if (confirmPrinciple.customId === 'left') {
+        target = principles[0];
+        balanceMod = -pc.balance;
+      } else {
+        target = principles[1];
+        balanceMod = pc.balance;
+      }
+
+      await confirmPrinciple.update({
+        content: `Chosen principle: ${target}`,
+        components: [],
+      });
+    } else if (pc) {
+      const { principles } = pc.getPlaybook();
+      const idx = principles.indexOf(
+        principles.find(p => p.toLowerCase() === target.toLowerCase())
+      );
+
+      if (idx === -1) {
+        return await interaction.followUp(
+          `${target} is not a valid principle. Choose ${principles[0]} or ${principles[1]}.`
+        );
+      } else {
+        target = principles[idx];
+        balanceMod = pc.balance;
+        if (idx === 0) balanceMod *= -1;
+      }
+    }
+
+    const roll = new Roll();
 
     if (pc) {
+      roll.addModifier(balanceMod, target);
+
       const markedConditions = pc.conditionList();
       if (markedConditions.includes('Guilty')) {
         roll.addModifier(2, 'Guilty');
       } else if (markedConditions.includes('Worried')) {
         roll.addModifier(2, 'Worried');
       }
+    }
+
+    if (override === null) {
+      roll.addModifier(interaction.options.getInteger('extra-modifier'));
+    } else {
+      roll.addModifier(override);
     }
 
     await interaction.followUp(
